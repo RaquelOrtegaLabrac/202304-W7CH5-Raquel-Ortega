52c8186c2db8442d2e1200f93998b2c9536f4153
labrac <raquelortegalabrac@hotmail.com>
2023-06-17 11:54:54 +0200

    Add user.mongo.model.ts

---
+++ src/repository/user.mongo.model.ts
@@ -0,0 +1,29 @@
+import { Schema, model } from 'mongoose';
+import { User } from '../entities/user.js';
+
+const userSchema = new Schema<User>({
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.set('toJSON', {
+  transform(_document, returnedObject) {
+    returnedObject.id = returnedObject._id;
+    delete returnedObject.password;
+    delete returnedObject.email;
+  },
+});
+
+export const UserModel = model('User', userSchema, 'users');
