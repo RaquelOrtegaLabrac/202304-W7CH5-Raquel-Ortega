53443d642e1086ae74500fe76e65ef5380eb2a5a
labrac <raquelortegalabrac@hotmail.com>
2023-06-18 17:11:20 +0200

    Update uri in db.connect.ts

--- src/db/db.connect.ts
+++ src/db/db.connect.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
-import { user, passwd, db } from '../config.js';
+import { user, password, db } from '../config.js';
 
 export const dbConnect = () => {
-  const uri = `mongodb+srv://${user}:${passwd}@cluster0.p2bwofa.mongodb.net/${db}?retryWrites=true&w=majority`;
+  const uri = `mongodb+srv://${user}:${password}@cluster0.ojlpryl.mongodb.net/${db}?retryWrites=true&w=majority`;
   return mongoose.connect(uri);
 };
