4e38cf5019791d9bac9dfa0d8a382beadb03af87
labrac <raquelortegalabrac@hotmail.com>
2023-06-20 17:49:10 +0200

    Update user router

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -4,13 +4,25 @@ import { User } from '../entities/user.js';
 import { UserRepo } from '../repository/user.mongo.repository.js';
 import { UserController } from '../controllers/user.controller.js';
 import { Repo } from '../repository/repo.js';
+import { ValidationMiddleware } from '../middleware/validation.js';
+import { FileMiddleware } from '../middleware/files.js';
 const debug = createDebug('W6:UserRouter');
 
 debug('Executed');
 const repo: Repo<User> = new UserRepo() as Repo<User>;
 const controller = new UserController(repo);
+
+const fileStore = new FileMiddleware();
+const validation = new ValidationMiddleware();
 export const userRouter = createRouter();
 
 userRouter.get('/', controller.getAll.bind(controller));
-userRouter.post('/register', controller.register.bind(controller));
+userRouter.post(
+  '/register',
+  fileStore.singleFileStore('avatar').bind(fileStore),
+  validation.registerValidation().bind(validation),
+  fileStore.optimization.bind(fileStore),
+  fileStore.saveDataImage.bind(fileStore),
+  controller.register.bind(controller)
+);
 userRouter.patch('/login', controller.login.bind(controller));
