9cd3e62eefca20b523eb99b33c6600816662c852
labrac <raquelortegalabrac@hotmail.com>
2023-06-20 18:00:02 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -5,3 +5,11 @@ export const user = process.env.DB_USER;
 export const passwd = process.env.DB_PASSWORD;
 export const db = process.env.DB_NAME;
 export const secret = process.env.JWT_SECRET;
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDjCs-Y1M5_ZrOkqi027QAPZfqZ4Zk6Ijw',
+  authDomain: 'proyecto-manhattan-3ba91.firebaseapp.com',
+  projectId: 'proyecto-manhattan-3ba91',
+  storageBucket: 'proyecto-manhattan-3ba91.appspot.com',
+  messagingSenderId: '370645271028',
+  appId: '1:370645271028:web:aea45bcd2697b6c573b9a1',
+};
