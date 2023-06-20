7eea8a7694a4cb4f7dd45d2ac25a457b92f90bdb
labrac <raquelortegalabrac@hotmail.com>
2023-06-20 17:28:10 +0200

    Add userSchema and avatar to user.ts

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,11 +1,34 @@
+import joi from 'joi';
+
+export type Avatar = {
+  urlOriginal: string;
+  url: string;
+  mimetype: string;
+  size: number;
+};
+
 export type User = {
   id: string;
   userName: string;
   email: string;
   password: string;
+  avatar: Avatar;
 };
 
 export type UserLogin = {
   user: string;
   password: string;
 };
+
+export const userSchema = joi.object<User>({
+  userName: joi.string().required(),
+  email: joi.string().email().required().messages({
+    'string.base': `"email" debe ser tipo 'texto'`,
+    'string.email': `El "email"  no es válido`,
+    'string.empty': `El "email" no puede faltar`,
+  }),
+  password: joi
+    .string()
+    .pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)
+    .required(),
+});
